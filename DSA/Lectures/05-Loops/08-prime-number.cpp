b198ed1c50968e1b85cc96dc407c3454e4c16497
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-10-25 17:29:43 +0500

    Add prime number checker program using basic trial division

---
+++ DSA/Lectures/05-Loops/08-prime-number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int n;
+  cout << "Enter a number: ";
+  cin >> n;
+
+  bool isPrime = true;
+
+  for (int i = 2; i < n; i++) {
+    if (n % i == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  if (isPrime) {
+    cout << "Prime Number." << endl;
+  } else {
+    cout << "Not Prime." << endl;
+  }
+
+  return 0;
+}
