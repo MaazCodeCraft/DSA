97dd37c9f93c51651249033959f3adb6247cce71
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-10-26 12:32:19 +0500

    Add hollow rectangle pattern implementation

---
+++ DSA/Lectures/06-Patterns/06-hollow-rectangle-pattern.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int n;
+  cout << "Enter a Number : ";
+  cin >> n;
+
+  for (int i = 1; i <= n; i++) {
+    cout << "*";
+    for (int j = 1; j < n; j++) {
+      if (i == 1 || i == n) {
+        cout << "*";
+      } else {
+        cout << " ";
+      }
+    }
+    cout << "*\n";
+  }
+
+  return 0;
+}
