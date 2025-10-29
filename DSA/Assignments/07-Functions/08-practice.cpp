c966b704aba7e19dd2a291407af879abb56b010e
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-10-29 15:35:02 +0500

    Add prime number generator with isPrime helper function

---
+++ DSA/Assignments/07-Functions/08-practice.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+  if (n == 1) {
+    return false;
+  }
+  for (int i = 2; i * i <= n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void allPrimeNum(int n) {
+  for (int i = 2; i <= n; i++) {
+    if (isPrime(i)) {
+      cout << i << " ";
+    }
+  }
+  cout << endl;
+}
+
+int main() {
+  int n;
+  cout << "Enter a number: ";
+  cin >> n;
+
+  allPrimeNum(n);
+  return 0;
+}
