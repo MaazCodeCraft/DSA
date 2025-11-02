f93f43f202f9bbefb22534b063e2ab39649b5e45
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-11-02 12:50:05 +0500

    Add initial implementation demonstrating pointer assignment and address output

---
+++ DSA/Assignments/09-Pointers/01-practice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5, y = 10;
+  int *ptr1 = &x, *ptr2 = &y;
+  ptr2 = ptr1;
+
+  cout << ptr1 << " = " << ptr2 << " = " << &x << endl;
+  return 0;
+}
