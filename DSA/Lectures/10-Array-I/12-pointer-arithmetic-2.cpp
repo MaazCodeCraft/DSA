5660a9329626fad26575ceaea60c4dc87aae4fb6
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-11-03 16:13:05 +0500

    Add pointer arithmetic example for array traversal

---
+++ DSA/Lectures/10-Array-I/12-pointer-arithmetic-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void printArr(int *arr, int n) {
+  for (int i = 0; i < n; i++) {
+    cout << *(arr + i) << endl;
+  }
+}
+
+int main() {
+  int arr[5] = {1, 2, 3, 4, 5};
+
+  int n = sizeof(arr) / sizeof(int);
+  printArr(arr, n);
+
+  return 0;
+}
