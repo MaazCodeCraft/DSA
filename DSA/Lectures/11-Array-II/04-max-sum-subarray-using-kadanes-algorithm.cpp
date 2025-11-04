debf738fe788ff9bd2bc5451b91265aea895057c
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-11-04 17:50:30 +0500

    Add Kadane's algorithm O(n) implementation for maximum subarray sum

---
+++ DSA/Lectures/11-Array-II/04-max-sum-subarray-using-kadanes-algorithm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void maxSubarraySum(int *arr, int n) { // TC: O(n)
+  int maxSum = INT_MIN;
+  int currSum = 0;
+  for (int i = 0; i < n; i++) {
+    currSum += arr[i];
+    maxSum = max(maxSum, currSum);
+    if (currSum < 0) {
+      currSum = 0;
+    }
+  }
+  cout << "Maximum Subarray Sum = " << maxSum << endl;
+}
+
+int main() {
+  int arr[] = {2, -3, 6, -5, 4, 2};
+  int n = sizeof(arr) / sizeof(int);
+  maxSubarraySum(arr, n);
+  return 0;
+}
