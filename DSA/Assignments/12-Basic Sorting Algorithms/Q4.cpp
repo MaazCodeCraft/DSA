32e3d35728061ecc2ed42c9c937ff97cdedb414e
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-11-09 12:33:35 +0500

    Add counting sort implementation in descending order for Q4

---
+++ DSA/Assignments/12-Basic Sorting Algorithms/Q4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void printArr(int *arr, int n) {
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+}
+
+void countingSort(int *arr, int n) {
+  int freq[n] = {0};
+  int minVal = INT_MAX, maxVal = INT_MIN;
+
+  for (int i = 0; i < n; i++) {
+    freq[arr[i]]++;
+    minVal = min(minVal, arr[i]);
+    maxVal = max(maxVal, arr[i]);
+  }
+
+  for (int i = maxVal, j = 0; i >= minVal; i--) {
+    while (freq[i] > 0) {
+      arr[j++] = i;
+      freq[i]--;
+    }
+  }
+  printArr(arr, n);
+}
+
+int main() {
+  int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
+  int n = sizeof(arr) / sizeof(int);
+  countingSort(arr, n);
+  return 0;
+}
