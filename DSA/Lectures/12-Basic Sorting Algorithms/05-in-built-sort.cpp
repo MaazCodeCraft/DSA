ac9c52b919adaceab131ec6a75a00fdf6bf12b02
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-11-09 11:33:39 +0500

    Add demonstration of C++ built-in sort function with ascending and descending order

---
+++ DSA/Lectures/12-Basic Sorting Algorithms/05-in-built-sort.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void printArr(int *arr, int n) {
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+int main() {
+  int arr[] = {1, 4, 1, 3, 2, 4, 3, 7};
+  int n = sizeof(arr) / sizeof(int);
+  sort(arr, arr + n);
+  printArr(arr, n);
+  sort(arr, arr + n, greater<int>());
+  printArr(arr, n);
+  return 0;
+}
