008e8f8c2832181856fa074f38075d266d564176
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-11-09 12:00:06 +0500

    Add Q1 bubble sort implementation with descending order sorting

---
+++ DSA/Assignments/12-Basic Sorting Algorithms/Q1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void swapping(int &a, int &b) {
+  int temp = a;
+  a = b;
+  b = temp;
+}
+
+void printArr(int *arr, int n) {
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+}
+
+void bubbleSort(int *arr, int n) {
+  for (int i = 0; i < n - 1; i++) {
+    bool isSwap = false;
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        swapping(arr[j], arr[j + 1]);
+        isSwap = true;
+      }
+      if (!isSwap) {
+        break;
+      }
+    }
+  }
+  printArr(arr, n);
+}
+
+int main() {
+  int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
+  int n = sizeof(arr) / sizeof(int);
+  bubbleSort(arr, n);
+  return 0;
+}
