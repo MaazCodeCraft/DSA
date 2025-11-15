261196ebf8a5c96a715c575226a716d8545da311
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-11-15 15:28:59 +0500

    Add string comparison operators demonstration program

---
+++ DSA/Lectures/14-Char Arrays & String/12-strings-operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string str1 = "cat";
+  string str2 = "dog";
+
+  cout << boolalpha << (str1.length() == str2.length()) << endl;
+  cout << boolalpha << (str1 == str2) << endl;
+  cout << boolalpha << (str1 > str2) << endl;
+  cout << boolalpha << (str1 < str2) << endl;
+  cout << boolalpha << (str1 != str2) << endl;
+
+  return 0;
+}
