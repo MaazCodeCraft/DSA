b7274e82685f46ee7eb2e3d9cb4e34c0d1ec8b83
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-11-30 12:38:45 +0500

    Add recursive power function with O(n) time complexity

---
+++ DSA/Assignments/19-Recursion Part-I/Q2-print-x-to-the-power-n-O(n).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int power(int x, int n) {
+  if (n == 0) {
+    return 1;
+  }
+  return x * power(x, n - 1);
+}
+
+int main() {
+  int x = 2, n = 10;
+  cout << power(x, n) << endl;
+  return 0;
+}
