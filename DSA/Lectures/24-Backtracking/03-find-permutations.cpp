b79ad6671f9d2cfee3dc7a5a75c01239383ac966
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-12-29 17:49:48 +0500

    Add backtracking algorithm to find all permutations of a string

---
+++ DSA/Lectures/24-Backtracking/03-find-permutations.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void permutations(string str, string ans) { // TC:-> O(n!), SC:-> O(n)
+  int n = str.size();
+  if (n == 0) {
+    cout << ans << endl;
+    return;
+  }
+
+  for (int i = 0; i < n; i++) {
+    char ch = str[i];
+    string nextStr = str.substr(0, i) + str.substr(i + 1, n - i - 1);
+    permutations(nextStr, ans + ch); // ith char choice to add in parm
+  }
+}
+
+int main() {
+  string str = "abc";
+  string ans = "";
+
+  permutations(str, ans);
+  return 0;
+}
