969c3897451dd69b30dc343953148ba55c63882a
maazurrahman <maaz4cs@gmail.com>
2026-01-13 17:52:32 +0500

    Add safety checks to n-queens backtracking algorithm

--- DSA/Lectures/24-Backtracking/04-n-queens.cpp
+++ DSA/Lectures/24-Backtracking/04-n-queens.cpp
@@ -14,23 +14,61 @@ void printBoard(vector<vector<char>> board) {
   cout << "----------------" << endl;
 }
 
+bool isSafe(vector<vector<char>> board, int row, int col) {
+  int n = board.size();
+
+  // 1-> Horizontal Safety
+  for (int j = 0; j < n; j++) {
+    if (board[row][j] == 'Q') {
+      return false;
+    }
+  }
+
+  // 2-> Vertical Safety
+  for (int i = 0; i < row; i++) {
+    if (board[i][col] == 'Q') {
+      return false;
+    }
+  }
+
+  // 2-> Diagonal Left Safety
+  for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+    if (board[i][j] == 'Q') {
+      return false;
+    }
+  }
+
+  // 2-> Diagonal right Safety
+  for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
+    if (board[i][j] == 'Q') {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 void nQueens(vector<vector<char>> board, int row) {
   int n = board.size();
+
+  // BC
   if (row == n) {
     printBoard(board);
     return;
   }
 
   for (int j = 0; j < n; j++) { // cols
-    board[row][j] = 'Q';
-    nQueens(board, row + 1);
-    board[row][j] = '.';
+    if (isSafe(board, row, j)) {
+      board[row][j] = 'Q';
+      nQueens(board, row + 1);
+      board[row][j] = '.';
+    }
   }
 }
 
 int main() {
   vector<vector<char>> board;
-  int n = 2;
+  int n = 4;
 
   for (int i = 0; i < n; i++) {
     vector<char> newRow;
